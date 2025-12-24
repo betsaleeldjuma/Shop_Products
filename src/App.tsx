f923fd572552ff24d88d34c3194c92642df48c85
betsaleeldjuma <djumabestaleel@gmail.com>
2025-12-24 18:54:38 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to={} />} />
+      <Route path="/" element={} />
+      <Route path="/" element={} />
+      <Route path="/" element={} />
+    </Routes>
+  )
+}
+
+export default App
