d74d6e074e8c57ae270a9a26b2a09123ee58b67c
betsaleeldjuma <djumabestaleel@gmail.com>
2025-12-24 19:20:32 +0200

    Add of form

---
+++ src/components/Form.tsx
@@ -0,0 +1,15 @@
+const Form = () => {
+  return (
+    <div>
+        <label>Name:</label>
+        <input type="text" />
+        <label>Post-Name:</label>
+        <input type="text"/>
+        <label>Password:</label>
+        <input type="password" />
+        <button>Login</button>
+    </div>
+  )
+}
+
+export default Form
