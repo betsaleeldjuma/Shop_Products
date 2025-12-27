552e830f866f7e3e53e42c7a934add673235a8de
betsaleeldjuma <djumabestaleel@gmail.com>
2025-12-27 09:28:47 +0200

    Update

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,6 +1,39 @@
+import { useQuery } from "@tanstack/react-query"
+import { useParams } from "react-router-dom"
+import apiShop from "../api/apiShop"
+
+interface Product {
+    id: number
+    title: string
+    category: string
+    price: number
+    rating: number
+    description: string
+}
+
+const fetchProduct = async (id: number): Promise<Product> => {
+    const res = await apiShop(`/products/${id}`)
+
+    return res.data
+}
+
 const Description = () => {
+    const {id} = useParams<{id:string}>()
+    const productId = id ? Number(id) : undefined
+    const {data, isError, isLoading} = useQuery<Product>({queryKey: ['products', id], queryFn: () => fetchProduct(productId!), enabled: !!id})
+
+    if(isLoading) return <div><h1>Loading...</h1></div>
+    if(isError) return <div><p>An Error Occured</p></div>
+    if(!data) return <div><h1>No Products Found</h1></div>
+
   return (
-    <div>Description</div>
+    <div>
+        <h1>{data.title}</h1>
+        <p>Price: ${data.price}</p>
+        <p>Category: {data.category}</p>
+        <h2>Description</h2>
+        <p>{data.description}</p>
+    </div>
   )
 }
 
